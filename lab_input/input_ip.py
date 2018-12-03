27b7b3a0988cd3573d6363358381fa688237a6f1
texassmak <jeremiah.mcatee3@verizonwireless.com>
2018-12-03 13:31:09 -0500

    finished lab7

---
+++ lab_input/input_ip.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Written by: Alice Anderson
+# Collect IP address information from a user, and display it back to them.
+
+## Collect input from the user
+user_input = input('Please enter an IPv4 address:')
+
+## Print out the input collected from the user
+print("You told me the IPV4 address is:" + user_input)
+
+
+## Collect vendor name associated to the device
+vendor_name = input('Please enter the vendor name associated with the device:')
+
+## Print out vendor name collected from user
+print("You told me the Vendor for your device is:" + vendor_name)
+
