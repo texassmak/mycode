a3dd9686b464ffb95f7d1483ffd46027ef7218f6
texassmak <jeremiah.mcatee3@verizonwireless.com>
2018-12-04 14:40:47 -0500

    redid final project lab 13

--- if-test/if-hostname.py
+++ if-test/if-hostname.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
-hostname = input('What is your hostname: ' ) # has user input a hostname
-if hostname in 'mtg' or 'MTG':  # should allow any version of mtg to be input
-	print('The hostname was found to be: ' + hostname) # acknowledges receipt of hostname input	
-	print('The hostname matches expected config') #provides result for if statement
+hostname = input('What is your hostname: ') # has user input a hostname
+if hostname == 'mtg' or hostname == 'MTG' or hostname == 'mTg' or hostname == 'MTg' or hostname == 'mTG' or hostname == 'MtG':  # should allow any version of mtg to be input
+    print('The hostname was found to be: ' + hostname) # acknowledges receipt of hostname input	
+    print('The hostname matches expected config') #provides result for if statement
+else: 
+    print('your hostname does not meet expected criteria')
 print('Exiting the script') # will advise you the script is ended
 
